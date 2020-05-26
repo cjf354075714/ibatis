95e5ff6f833d35b4c00ca5c6c4c674c6e25a4ba8
caojfc <caojfc@yonyou.com>
2020-05-26 20:41:08 +0800

    init

---
+++ src/main/java/cn/edu/cqu/learn/architecture/ibatis/IbatisApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cqu.learn.architecture.ibatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IbatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/cqu/learn/architecture/ibatis/IbatisApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.cqu.learn.architecture.ibatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
