df03a0b6afaeaad93164b208746095afc550506a
caojfc <caojfc@yonyou.com>
2020-07-03 22:00:48 +0800

    SqlSession 待学习

--- src/main/resources/readme.md
+++ src/main/resources/readme.md
@@ -1,3 +1,8 @@
-Mybatis内部核心组件：
-xml解析组件，现在不看
-Reflect 组件，目的是去实现类JAVA Bean 的构造
+## 流程
+
+* XML 工具类将去读取 Mybatis 的配置文件，我们不需要关注这个构建细节，只需要知道构建就行了
+* 构建之后将返回一个对象，里面包含了 Mybatis 的所有核心配置，Configuration类
+* 这个类将包含 Enviroment 对象，存的是数据库连接池对象
+* SqlSessionFactoryBuilder 将根据这个 Enviroment 对象，去返回一个SqlSessionFactory
+* SqlSessionFactory 将返回 SqlSession，SqlSession本质上就是 Connection 的包装，就是通过 Enviroment去构建的
+* 
--- src/test/java/cn/edu/cqu/learn/architecture/ibatis/IBatisApplicationTests.java
+++ src/test/java/cn/edu/cqu/learn/architecture/ibatis/IBatisApplicationTests.java
@@ -1,7 +1,16 @@
 package cn.edu.cqu.learn.architecture.ibatis;
 
+import org.apache.ibatis.mapping.Environment;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.io.InputStream;
+import java.sql.Connection;
 
 @SpringBootTest
 class IBatisApplicationTests {
@@ -10,4 +19,46 @@ class IBatisApplicationTests {
     void contextLoads() {
     }
 
+
+    // 通过文件流去构建一个 SqlSessionFactory 对象
+
+    // 比如一个节点，Environment 节点，首先是通过 XML 工具类，去读取 Environment 节点
+    // 然后根据这个节点的配置去生成一个 DataSource 对象，我们不需要去关注细节
+    // 只需要知道，根据配置文件，写死的，比如，配置文件写的是 POOLED
+    // 对象就返回一个 PooledConnection 对象，本质上是 DataSource 的实现类
+    // 构建的过程有些细节
+    @Test
+    SqlSessionFactory sqlSessionFactory() {
+        return new SqlSessionFactoryBuilder().build((InputStream) null);
+    }
+
+    // 这个对象包含了所有的 Mybatis 的基础信息
+    // 在构建 SqlSessionFactory 的时候，需要用到里面的 Environment 对象
+    // 这个对象，将被传输给 SqlSession。而且，所有的 SqlSession 的将用同一个 Configuration
+    @Test
+    Configuration configuration() {
+        return new Configuration();
+    }
+
+    // 包含 ID、DataSource、TransactionFactory
+    // DataSource 对象，就是我们接触到的普通对象
+    // Mybatis 会有自己的一些实现对象
+    // TransactionFactory 是 Mybatis 的事务管理器工厂
+    // 事务涉及到什么呢？事务的隔离级别，和是否自定提交，
+    // MyBatis 根据这个特性，封装了一个事务对象，就包含三个值：Connection、隔离界别、是否自动提交
+    // 就是一个包装对象
+    @Test
+    Environment environment() {
+        return new Environment(null, null, null);
+    }
+
+    // 通过 SqlSessionFactory 打开 SqlSession 对象
+    // SqlSession 是 Transaction 的包装类，此外还有一些其他对象
+    // Configuration 该对象将被用作 Sql 执行之后，做一些事情
+    // Executor 该对象是 Sql 的真正执行器
+
+    @Test
+    SqlSession sqlSession() {
+        return sqlSessionFactory().openSession((Connection) null);
+    }
 }
