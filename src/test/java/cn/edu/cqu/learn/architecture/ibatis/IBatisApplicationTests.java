ee8a69a6ae66097d50ad34e3bd7551b586d8f120
曹俊锋 <caojfc@yonyou.com>
2020-07-18 11:12:55 +0800

    新增url对象的理解

--- src/test/java/cn/edu/cqu/learn/architecture/ibatis/IBatisApplicationTests.java
+++ src/test/java/cn/edu/cqu/learn/architecture/ibatis/IBatisApplicationTests.java
@@ -6,6 +6,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.EnumMap;
+import java.util.Map;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
 import java.util.*;
@@ -35,6 +40,32 @@ class IBatisApplicationTests {
         enumMap.put(JdbcType.CHAR, "char");
         for (Map.Entry<JdbcType, Object> index : enumMap.entrySet()) {
             SLF4J.info(index.toString());
+        }
+    }
+
+    // URL 类，我一直看不懂，在弄懂这个之前，需要了解什么是同一资源定位符
+    // 在网络上，任何资源文件，都存在一个电脑中，为了标识出他们的具体位置
+    // 需要有一些协议，来制定，然后这些协议，用的是同一的字符串表示
+    // 这个字符串就是 URL
+    // 之前的 http 协议等，都可以纳入到这个 URL 中
+    // URL 的具体格式：
+    // protocol://hostname[:port]/path/[?params]
+    // 主要协议有：file、http、ftp、jdbc?等
+    // 我们具体关注 file、http
+    // file的 url 格式为：file:///d/index.html
+    // 为什么 file 协议是三条 / 因为，人家系统的盘符都是挂在到 '/' 下的，就 win 是直接 c
+
+    // 即使是我们使用了 URL，这个类又有什么作用呢
+
+    // getFile() 返回这个协议，所指定的具体的路径名，我们可以用它构建一个文件对象
+    @Test
+    void urlTest() {
+        try {
+            URL url = new URL("file:///d/cjf/index.html");
+            String filePath = url.toExternalForm();
+            SLF4J.info(url.toString());
+        } catch (MalformedURLException e) {
+            SLF4J.error("加载 URL 失败：{}", e.getMessage());
         }
     }
 
